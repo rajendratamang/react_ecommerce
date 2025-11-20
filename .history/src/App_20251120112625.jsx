f616723252814200368264dd67b10cc84d62d752
Rajendra Tamang <tamangrajendra0523@gmail.com>
2025-11-20 11:26:47 +0545

    Feat : checkout route created

--- .history/src/App_20251120112625.jsx
+++ .history/src/App_20251120112625.jsx
@@ -3,7 +3,6 @@ import Home from './components/Home';
 import Shop from './components/Shop';
 import Product from './components/Product';
 import Cart from './components/Cart';
-import Checkout from './components/Checkout';
 
 function App() {
   return (
@@ -13,7 +12,7 @@ function App() {
         <Route path="/shop" element={<Shop />} />
         <Route path="/product" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/checkout" element={<Cart />} />
       </Routes>
     </BrowserRouter>
   );
