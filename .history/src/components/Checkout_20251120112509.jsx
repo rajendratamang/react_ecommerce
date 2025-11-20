491edc8e4b113cdc85d9f7a552be1d46e4b7663c
Rajendra Tamang <tamangrajendra0523@gmail.com>
2025-11-20 11:25:57 +0545

    Feat: Checkout file created

---
+++ .history/src/components/Checkout_20251120112509.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from './common/Layout'
+
+const Checkout = () => {
+  return (
+Layout
+  )
+}
+
+export default Checkout
---
+++ src/components/Checkout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from './common/Layout'
+
+const Checkout = () => {
+  return (
+<Layout>
+    
+</Layout>
+  )
+}
+
+export default Checkout
